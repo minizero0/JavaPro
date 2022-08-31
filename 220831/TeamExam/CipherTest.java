2e927a83119031e8d01ea06425626c444b32dd60
yeongmin <dudals1069@gmail.com>
2022-08-31 17:17:18 +0900

    0831

---
+++ 220831/TeamExam/CipherTest.java
@@ -0,0 +1,34 @@
+package TeamExam;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class CipherTest {
+
+	public static void main(String[] args) {
+		
+		try {
+			FileReader fr = new FileReader(args[0]);
+			FileWriter fw = new FileWriter(args[1]);
+			
+			int ch;
+			String str = "";
+			while ((ch = fr.read()) != -1) {
+				if (ch >= 120 && ch <= 122)
+					ch -= 55;
+				else if (ch == 32)
+					ch = 32;
+				else
+					ch -= 29;
+				str += (char)ch;
+			}
+			fw.write(str);
+			fr.close();
+			fw.close();
+		}catch (Exception e) {
+			System.out.println("예외발생:" + e.getMessage());
+		}
+
+	}
+
+}
--- 220831/exam02/ConcatTest.java
+++ 220831/exam02/ConcatTest.java
@@ -6,14 +6,11 @@ import java.io.FileWriter;
 public class ConcatTest {
 
 	public static void main(String[] args) {
-		String s1 = args[0];
-		String s2 = args[1];
-		String s3 = args[2];
 		
 		try {
-			FileReader fr1 = new FileReader(s1);
-			FileReader fr2 = new FileReader(s2);
-			FileWriter fw = new FileWriter(s3);
+			FileReader fr1 = new FileReader(args[0]);
+			FileReader fr2 = new FileReader(args[1]);
+			FileWriter fw = new FileWriter(args[2]);
 			
 			int ch;
 			String str = "";
