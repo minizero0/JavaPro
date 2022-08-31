234c3dd5aa0d5a35f649a5ed968b81409526bd3c
yeongmin <dudals1069@gmail.com>
2022-08-31 16:15:24 +0900

    0831

--- 220831/exam02/FileCopy.java
+++ 220831/exam02/FileCopy.java
@@ -1,10 +1,34 @@
 package exam02;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
+//java FileCopy a.txt b.txt
 public class FileCopy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String from = args[0];
+		String to = args[1];
+		
+		try {
+			FileReader fr = new FileReader(from);
+			FileWriter fw = new FileWriter(to);
+			
+			int ch;
+			String str = "";
+			while ((ch = fr.read()) != -1) {
+				str += (char)ch;
+			}
+			
+			fw.write(str);
+			
+			fr.close();
+			fw.close();
+			
+			System.out.println("파일복사완료.");
+		}catch(Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
 	}
 
 }
