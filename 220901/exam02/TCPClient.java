79bf6d05d2167e2052b4172ad2375a4651c9ae49
yeongmin <dudals1069@gmail.com>
2022-09-01 12:57:40 +0900

    0901

--- 220901/exam02/TCPClient.java
+++ 220901/exam02/TCPClient.java
@@ -7,11 +7,15 @@ public class TCPClient {
 
 	public static void main(String[] args) {
 		try {
-			Socket socket = new Socket("192.168.0.120", 9000);//서버 아이피 주소 , 포트번호 
+			Socket socket = new Socket("192.168.0.120", 12000);//서버 아이피 주소 , 포트번호 
 			InputStream is = socket.getInputStream();
 			for (int i = 0; i < 10; i++) {
 				System.out.print(is.read() + " ");
 			}
+			System.out.println(socket.getPort());
+			is.close();
+			socket.close();
+			
 			
 		}catch (Exception e) {
 			System.out.println("예외발생:" + e.getMessage());
--- 220901/exam02/TCPServer.java
+++ 220901/exam02/TCPServer.java
@@ -7,8 +7,12 @@ import java.net.Socket;
 public class TCPServer {
 
 	public static void main(String[] args) {
+		System.out.println("Server Started");
 		try {
-			ServerSocket server = new ServerSocket(9000);
+			ServerSocket server = new ServerSocket(12000);
+			
+			System.out.println("Client connect");
+			Thread.sleep(3000);
 			while(true) {
 				Socket socket = server.accept();
 				OutputStream os = socket.getOutputStream();
@@ -18,6 +22,8 @@ public class TCPServer {
 					os.write(r);
 					Thread.sleep(100);
 				}
+				os.close();
+				socket.close();
 			}
 		}catch (Exception e) {
 			System.out.println("예외발생:" + e.getMessage());
