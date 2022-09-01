4d3b176aa93e74790a6a80b7d02d56632b1cc917
yeongmin <dudals1069@gmail.com>
2022-09-01 10:07:15 +0900

    0901

---
+++ 220901/homework/DeCipher_Map.java
@@ -0,0 +1,46 @@
+package homework;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+
+public class DeCipher_Map {
+
+	public static void main(String[] args) {
+		try {
+			FileReader fr = new FileReader(args[0]);
+			FileWriter fw = new FileWriter(args[1]);
+			
+			String key = "DEFGHIJKLMNOPQRSTUVWXYZABC";
+			String value = "abcdefghijklmnopqrstuvwxyz";
+			
+			
+			HashMap<String, String> map = new HashMap<>();
+			for (int i = 0; i < key.length(); i++) {
+				map.put(key.charAt(i)+"", value.charAt(i)+"");
+			}
+			
+			String str = "";
+			String src = "";
+			int ch;
+			while((ch = fr.read()) != -1) {
+				if (ch >= 'a' && ch <= 'z') {
+					String s = ((char)ch)+"";
+					str += map.get(s);
+				}
+				else {
+					str += (char)ch;
+				}
+			}
+			
+			fw.write(str);
+			fr.close();
+			fw.close();
+			System.out.println("변환성공");
+			
+		}catch(Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
+	}
+
+}
