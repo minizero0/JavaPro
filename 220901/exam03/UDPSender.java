c004fb52c977f79a18c850cfaf6d9d7e390048d9
yeongmin <dudals1069@gmail.com>
2022-09-01 16:25:35 +0900

    0901

--- 220901/exam03/UDPSender.java
+++ 220901/exam03/UDPSender.java
@@ -1,5 +1,5 @@
 package exam03;
-
+	
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -10,7 +10,8 @@ public class UDPSender {
 		try {
 			DatagramSocket socket = new DatagramSocket();
 			InetAddress add = InetAddress.getByName(args[0]);
-			byte data[] = args[1].getBytes();
+			String s = "Mr.cho";
+			byte data[] = s.getBytes();
 			DatagramPacket packet = new DatagramPacket(data, data.length, add, 13001);
 			
 			socket.send(packet);
