39c986ebcbc2b12ba2c2201d6d2394b345c1cc09
yeongmin <dudals1069@gmail.com>
2022-09-02 09:46:38 +0900

    0901

--- 220902/exam01/UDPEchoClient.java
+++ 220902/exam01/UDPEchoClient.java
@@ -1,4 +1,4 @@
-package exam04;
+package exam01;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,13 +17,13 @@ import javax.swing.JTextField;
 
 
 
-public class UDPEchoClient extends JFrame implements Runnable{
+public class UDPEchoClient extends JFrame {
 	private JTextArea jta;  		//멤버변수
 	private JTextField jtf;
 	DatagramSocket socket;
 	DatagramPacket packet;
 	InetAddress addr;
-	int port = 9004;
+	int port = 9005;
 	
 	
 	public UDPEchoClient(){
@@ -30,7 +31,7 @@ public class UDPEchoClient extends JFrame implements Runnable{
 		jtf = new JTextField();
 		try {
 			socket = new DatagramSocket();
-			addr = InetAddress.getByName("192.168.0.120");
+			addr = InetAddress.getByName("172.30.1.3");
 		}catch(Exception e) {
 			System.out.println("예외발생:" + e.getMessage());
 		}
@@ -49,6 +50,28 @@ public class UDPEchoClient extends JFrame implements Runnable{
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		class ClientThread extends Thread{
+			byte data[] = new byte[100];
+			DatagramPacket packet = new DatagramPacket(data, data.length);
+			public void run() {
+				while (true) {
+					try {
+						socket.receive(packet);
+						String msg = new String(data);
+						Arrays.fill(data, (byte)0);
+						jta.append(msg + "\n");
+					}catch(Exception e) {
+						System.out.println("예외발생:" + e.getMessage());
+					}
+					
+				}
+			}
+		}
+		
+		ClientThread ct = new ClientThread();
+		ct.start();
+		
+		
 		btn_send.addActionListener(new ActionListener() {
 			
 			@Override
@@ -59,9 +82,9 @@ public class UDPEchoClient extends JFrame implements Runnable{
 				packet = new DatagramPacket(data, data.length, addr, port);
 				try {
 					socket.send(packet);
-					socket.receive(packet);
-					String msg = new String(data);
-					jta.append(msg + "\n");
+//					socket.receive(packet);
+//					String msg = new String(data);
+//					jta.append(msg + "\n");
 //					jta.setText(msg);
 				}catch(Exception ex) {
 					System.out.println("예외발생:" + ex.getMessage());
@@ -70,29 +93,27 @@ public class UDPEchoClient extends JFrame implements Runnable{
 			}
 		});
 	}
-	@Override
-	public void run() {
-		try {
-			socket = new DatagramSocket();
-			byte data[] = new byte[100];
-			packet = new DatagramPacket(data, data.length);
-			
-			socket.receive(packet);
-			String msg = new String(data);
-			jta.append(msg + "\n");
-			
-			
-		}catch (Exception e) {
-			System.out.println("예외발생 : " + e.getMessage());
-		}
-	}
+//	@Override
+//	public void run() {
+//		try {
+//			socket = new DatagramSocket();
+//			byte data[] = new byte[100];
+//			packet = new DatagramPacket(data, data.length);
+//			
+//			socket.receive(packet);
+//			String msg = new String(data);
+//			jta.append(msg + "\n");
+//			
+//			
+//		}catch (Exception e) {
+//			System.out.println("예외발생 : " + e.getMessage());
+//		}
+//	}
 
 	
 
 	public static void main(String[] args) {
-		UDPEchoClient uec = new UDPEchoClient();
-		Thread t1 = new Thread(uec);
-		t1.start();
+		new UDPEchoClient();
 		
 		
 	}
---
+++ 220902/exam01/UDPEchoServer.java
@@ -0,0 +1,30 @@
+package exam01;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Arrays;
+
+public class UDPEchoServer {
+
+	public static void main(String[] args) {
+		try {
+			DatagramSocket socket = new DatagramSocket(9004);
+			
+			byte data[] = new byte[100];
+			DatagramPacket packet = new DatagramPacket(data, data.length);
+			
+			while(true) {
+				socket.receive(packet);
+				String msg = new String(data);
+				System.out.println("수신된 데이터 : " +msg);
+				socket.send(packet);
+				Arrays.fill(data, (byte)0);
+			}
+			
+		}catch(Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
+
+	}
+
+}
