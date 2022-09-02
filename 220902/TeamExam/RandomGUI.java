5e88d2fc3eedc5c222a84d232df29d4690e55af3
yeongmin <dudals1069@gmail.com>
2022-09-02 17:18:37 +0900

    0902

---
+++ 220902/TeamExam/RandomGUI.java
@@ -0,0 +1,63 @@
+package TeamExam;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class RandomGUI extends JFrame{
+	private int cnt = 0;
+	public RandomGUI() {
+		int r = (int)(Math.random() * 100 + 1);
+		setLayout(new BorderLayout());
+		JTextArea jta = new JTextArea();
+		JScrollPane jsp = new JScrollPane(jta);
+		JTextField jtf = new JTextField();
+		JButton btn = new JButton("입력");
+		JPanel jp = new JPanel(new BorderLayout());
+		
+		
+		jp.add(jtf, BorderLayout.CENTER);
+		jp.add(btn, BorderLayout.EAST);
+		
+		add(jsp, BorderLayout.CENTER);
+		add(jp, BorderLayout.SOUTH);
+		
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int n = Integer.parseInt(jtf.getText());
+				
+				if (r > n) 
+					jta.append("입력한 수" + (n+"") + "보다 높은 수를 입력하세요.\n");
+				else if(n == r) {
+					jta.append("정답\n입력 횟수:" + ++cnt + "");
+					return;
+				}
+				else
+					jta.append("입력한 수" + (n+"") + "보다 낮은 수를 입력하세요.\n");
+				cnt++;
+			}
+			
+		});
+		
+		
+		
+		
+		setSize(300,400);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new RandomGUI();
+	}
+
+}
