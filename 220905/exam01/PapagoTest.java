45dffc87e3b5d7023b4dd368b4d4949819ba3675
yeongmin <dudals1069@gmail.com>
2022-09-05 12:34:34 +0900

    0905

--- 220905/exam01/PapagoTest.java
+++ 220905/exam01/PapagoTest.java
@@ -3,9 +3,12 @@ package exam01;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -20,20 +23,37 @@ public class PapagoTest extends JFrame{
 	public PapagoTest() {
 		setLayout(new BorderLayout(50,50));
 		JPanel jp1 = new JPanel();
-		
 		JPanel jp2 = new JPanel();
-		
-		
 		
 		jp1.setLayout(new GridLayout(1,1,10,10));
 		jp2.setLayout(new FlowLayout());
 		
-		jta1 = new JTextArea("텍스트가 입력됩니다.\n몇개의 단어를 영어로\n 변환합니다.");
+		jta1 = new JTextArea("텍스트가 입력됩니다.\n몇개의 단어를 영어로\n변환합니다.");
 		JScrollPane jsp1 = new JScrollPane(jta1);
 		jta2 = new JTextArea("Text가 입력됩니다.\n몇개의 단어를 English로\n변환합니다.");
 		JScrollPane jsp2 = new JScrollPane(jta2);
 		btn1 = new JButton("변환");
 		btn2 = new JButton("취소");
+		
+		btn1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String str = jta1.getText();
+				str = str.replace("텍스트", "Text");
+				str = str.replace("영어", "English");
+				jta2.setText(str);
+			}
+		});
+		btn2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int r = JOptionPane.showConfirmDialog(null, "정말 종료하시겠습니까?");
+				if (r == 0)
+					System.exit(0);
+			}
+		});
 		
 		jp1.add(jsp1);
 		jp1.add(jsp2);
