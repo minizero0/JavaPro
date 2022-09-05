e48d189ad9964bd52b4d0d6cc947ff02c906c177
yeongmin <dudals1069@gmail.com>
2022-09-05 12:26:29 +0900

    0905

--- 220905/exam01/PapagoTest.java
+++ 220905/exam01/PapagoTest.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class PapagoTest extends JFrame{
@@ -16,23 +17,26 @@ public class PapagoTest extends JFrame{
 	JButton btn2;
 	
 	
-	
 	public PapagoTest() {
 		setLayout(new BorderLayout(50,50));
 		JPanel jp1 = new JPanel();
+		
 		JPanel jp2 = new JPanel();
+		
 		
 		
 		jp1.setLayout(new GridLayout(1,1,10,10));
 		jp2.setLayout(new FlowLayout());
 		
 		jta1 = new JTextArea("텍스트가 입력됩니다.\n몇개의 단어를 영어로\n 변환합니다.");
+		JScrollPane jsp1 = new JScrollPane(jta1);
 		jta2 = new JTextArea("Text가 입력됩니다.\n몇개의 단어를 English로\n변환합니다.");
+		JScrollPane jsp2 = new JScrollPane(jta2);
 		btn1 = new JButton("변환");
 		btn2 = new JButton("취소");
 		
-		jp1.add(jta1);
-		jp1.add(jta2);
+		jp1.add(jsp1);
+		jp1.add(jsp2);
 		jp2.add(btn1);
 		jp2.add(btn2);
 		
