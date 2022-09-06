2b93683a112900c63e6b88a54b31c3e4a0f94087
yeongmin <dudals1069@gmail.com>
2022-09-06 15:30:27 +0900

    0906

--- 220906/exam01/InsertMember.java
+++ 220906/exam01/InsertMember.java
@@ -1,7 +1,7 @@
 package exam01;
 
 import java.util.Scanner;
-
+//insert into member values('hong', ;010-111-111','서울시 마포구 서교동');
 public class InsertMember {
 
 	public static void main(String[] args) {
@@ -16,6 +16,8 @@ public class InsertMember {
 		addr = sc.next();
 		
 //		System.out.println("이름: " +name+ ",전화: " +phone+",주소: "+addr);
+		
+		String sql = "insert into member values('hong', ;010-111-111','서울시 마포구 서교동')";
 		
 		try {
 			
