6e899ac074d1762015bdce704f6adfaf23351581
yeongmin <dudals1069@gmail.com>
2022-09-06 15:35:55 +0900

    0906

--- 220906/exam01/InsertMember.java
+++ 220906/exam01/InsertMember.java
@@ -17,9 +17,14 @@ public class InsertMember {
 		
 //		System.out.println("이름: " +name+ ",전화: " +phone+",주소: "+addr);
 		
-		String sql = "insert into member values('hong', ;010-111-111','서울시 마포구 서교동')";
+		String sql = "insert into member values('"+name+"', '"+phone+"','"+addr+"')";
 		
 		try {
+			//1. jdbc(java database connection)드라이버를 메모리로 로드한
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//2. db Server에 연결한다.
+			
 			
 		}catch(Exception e) {
 			System.out.println("예외발생:"+e.getMessage());
