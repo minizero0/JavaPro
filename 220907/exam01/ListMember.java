d9faf5e2abbfd3a7c6eac5d938970e034a3fa754
yeongmin <dudals1069@gmail.com>
2022-09-07 10:44:07 +0900

    0907

--- 220907/exam01/ListMember.java
+++ 220907/exam01/ListMember.java
@@ -1,6 +1,11 @@
 package exam01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ListMember extends JFrame{
 
@@ -10,7 +15,18 @@ public class ListMember extends JFrame{
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
+			String url = "jdbc:oracle:thin:@172.30.1.3:1521:XE";
+			String usr = "c##sist0307";
+			String pwd = "sist0307";
+			Connection conn = DriverManager.getConnection(url, usr, pwd);
 			
+			Statement stmt = conn.createStatement();
+			
+			int re = stmt.executeUpdate(sql);
+			
+			if (re == 1) {
+				JOptionPane.showMessageDialog(null, "조회완료");
+			}
 			
 			
 		}catch(Exception e) {
