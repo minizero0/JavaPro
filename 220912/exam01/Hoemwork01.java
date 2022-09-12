184dee7ed26e7c142c1f1e86dffda5deb33830c0
yeongmin <dudals1069@gmail.com>
2022-09-12 22:04:57 +0900

    0912

--- 220912/exam01/Hoemwork01.java
+++ 220912/exam01/Hoemwork01.java
@@ -22,13 +22,14 @@ public class Hoemwork01 extends JFrame{
 	JTextField jtf;
 	JTable table;
 	Vector<Vector<String>> vector;
+	Vector<String> colName;
 	
-	public void search() {
+	public void list() {
 		vector.clear();
-		int id = Integer.parseInt(jtf.getText()); 
-		String sql = "select * from orders where memberid = "+id+"";
+		String sql = "select * from orders";
+		
 		try {
-			
+		
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			String url = "jdbc:oracle:thin:@172.30.1.3:1521:XE";
@@ -40,12 +41,48 @@ public class Hoemwork01 extends JFrame{
 			
 			ResultSet rs = stmt.executeQuery(sql);
 			while(rs.next()) {
-				Vector<String> v = new Vector<>();
-				v.add(rs.getInt(1)+"");
-				v.add(rs.getInt(2)+"");
-				v.add(rs.getInt(3)+"");
-				v.add(rs.getInt(4)+"");
-				vector.add(v);
+				Vector<String> v1 = new Vector<String>();
+				v1.add(rs.getInt(1)+"");
+				v1.add(rs.getInt(2)+"");
+				v1.add(rs.getInt(3)+"");
+				v1.add(rs.getInt(4)+"");
+				vector.add(v1);
+			}
+			conn.close();
+			stmt.close();
+			rs.close();
+			
+		}catch(Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
+		
+	}
+	
+	
+	public void search() {
+		vector.clear();
+		int id = Integer.parseInt(jtf.getText()); 
+		String sql = "select * from orders where memberid = "+id+"";
+		
+		try {
+		
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@172.30.1.3:1521:XE";
+			String user = "c##homework";
+			String pwd = "homework";
+			Connection conn = DriverManager.getConnection(url, user, pwd);
+			
+			Statement stmt = conn.createStatement();
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				Vector<String> v1 = new Vector<String>();
+				v1.add(rs.getInt(1)+"");
+				v1.add(rs.getInt(2)+"");
+				v1.add(rs.getInt(3)+"");
+				v1.add(rs.getInt(4)+"");
+				vector.add(v1);
 			}
 			conn.close();
 			stmt.close();
@@ -61,24 +98,18 @@ public class Hoemwork01 extends JFrame{
 		jtf = new JTextField(5);
 		JPanel jp = new JPanel();
 		jp.setLayout(new FlowLayout());
-		vector = new Vector<>();
+		vector = new Vector<Vector<String>>();
 		setLayout(new BorderLayout());
-		Vector<String> colName = new Vector<>();
+		colName = new Vector<>();
 		colName.add("주문번호");
 		colName.add("고객번호");
 		colName.add("도서번호");
 		colName.add("구매금액");
-		
-		JButton btn = new JButton("검색");
-		
-		jp.add(jtf);
-		jp.add(btn);
 		table = new JTable(vector, colName);
 		JScrollPane jsp = new JScrollPane(table);
+		list();
 		
-		add(jsp, BorderLayout.CENTER);
-		add(jp, BorderLayout.SOUTH);
-		
+		JButton btn = new JButton("검색");
 		btn.addActionListener(new ActionListener() {
 			
 			@Override
@@ -87,6 +118,14 @@ public class Hoemwork01 extends JFrame{
 				table.updateUI();
 			}
 		});
+		
+		jp.add(jtf);
+		jp.add(btn);
+		
+		add(jsp, BorderLayout.CENTER);
+		add(jp, BorderLayout.SOUTH);
+		
+	
 		
 		
 		setSize(300,400);
