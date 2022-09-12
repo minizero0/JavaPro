2e42f5d77dff635a7f5592c85e0450a91fe34ca6
yeongmin <dudals1069@gmail.com>
2022-09-12 21:17:20 +0900

    0907

--- 220912/exam01/Hoemwork01.java
+++ 220912/exam01/Hoemwork01.java
@@ -1,5 +1,19 @@
 package exam01;
 
-public class Hoemwork01 {
+import javax.swing.JFrame;
+
+import TeamExam.HomeWork01;
+
+public class Hoemwork01 extends JFrame{
+	
+	public HoemWork01() {
+		
+		setSize(300,400);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
