2b2a134579c23c9a0e6f916fc098d6662ae0176d
yeongmin <dudals1069@gmail.com>
2022-09-20 05:16:08 +0900

    0915

--- TeamProject1/frameTest/signin.java
+++ TeamProject1/frameTest/signin.java
@@ -1,15 +1,59 @@
 package frameTest;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class signin extends JFrame{
 	JTextField jtf_id, jtf_pwd;
 	JButton btn_signIn;
+	
+	public void login() {
+		String usrid = jtf_id.getText();
+		String usrpwd = jtf_pwd.getText();
+		
+		String sql = "select userid, userpwd from users";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@172.30.1.3:1521:XE";
+			String usr = "c##madang";
+			String pwd = "madang";
+			
+			Connection conn = DriverManager.getConnection(url, usr, pwd);
+			Statement stmt = conn.createStatement();
+			
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				System.out.println(rs.getString(1));
+				System.out.println(rs.getString(2));
+				System.out.println(usrid);
+				System.out.println(usrpwd);
+				if(rs.getString(1).equals(usrid) && rs.getString(2).equals(usrpwd)) {
+					JOptionPane.showMessageDialog(null, "로그인에 성공했습니다"+usrid+"님");
+				}
+				else
+					JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호가 옳바르지 않습니다.");
+			}
+			conn.close();
+			stmt.close();
+			rs.close();
+			
+		}catch (Exception e) {
+			System.out.println("예외발생:"+e.getMessage());
+		}
+		
+	}
+	
 	
 	public signin() {
 		setLayout(new GridLayout(3,2));
@@ -27,7 +71,14 @@ public class signin extends JFrame{
 		add(jtf_pwd);
 		add(btn_signIn);
 		
-		
+		btn_signIn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				login();
+				
+			}
+		});
 		
 		setSize(300,400);
 		setVisible(true);
