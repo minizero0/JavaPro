e6130262fa148effedb1fcd6568c6d3dd01574b1
yeongmin <dudals1069@gmail.com>
2022-09-21 17:08:12 +0900

    0815

--- 220921/exam/exam01.java
+++ 220921/exam/exam01.java
@@ -1,5 +1,8 @@
 package exam;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.swing.JFrame;
 
 public class exam01 extends JFrame{
@@ -9,7 +12,11 @@ public class exam01 extends JFrame{
 		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url 
+			String url = "jdbc:oracle:thin:@172.30.1.3:1521:XE";
+			String usr = "c##madang";
+			String pwd = "madang";
+			Connection conn = DriverManager.getConnection(url, usr, pwd);
+			
 			
 		}catch(Exception e) {
 			System.out.println("예외발생"+ e.getMessage());
