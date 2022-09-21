52dc58cbb207d38fd26df2e477f0f4919c7af85e
yeongmin <dudals1069@gmail.com>
2022-09-21 17:10:05 +0900

    0815

--- 220921/exam/exam01.java
+++ 220921/exam/exam01.java
@@ -2,6 +2,8 @@ package exam;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 
@@ -16,7 +18,7 @@ public class exam01 extends JFrame{
 			String usr = "c##madang";
 			String pwd = "madang";
 			Connection conn = DriverManager.getConnection(url, usr, pwd);
-			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 			
 		}catch(Exception e) {
 			System.out.println("예외발생"+ e.getMessage());
@@ -25,7 +27,11 @@ public class exam01 extends JFrame{
 	}
 	
 	public exam01() {
-		
+		Scanner sc = new Scanner(System.in);
+		int bookid = sc.nextInt();
+		String bookname = sc.next();
+		String publisher = sc.next();
+		int price = sc.nextInt();
 	}
 	
 	public static void main(String[] args) {
