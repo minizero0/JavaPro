4f37467da0f98673cd57ffdf6436e8a35439018b
yeongmin <dudals1069@gmail.com>
2022-09-21 17:05:11 +0900

    0815

--- 220921/exam/exam01.java
+++ 220921/exam/exam01.java
@@ -1,5 +1,20 @@
 package exam;
 
-public class exam01 {
+import javax.swing.JFrame;
+
+public class exam01 extends JFrame{
+	
+	public void book_insert() {
+		String sql = 
+		
+	}
+	
+	public exam01() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new exam01();
+	}
 
 }
